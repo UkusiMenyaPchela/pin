f9c579912e722d324cf9939cd4e892ead80c755d
UkusiMenyaPchela <99737044+UkusiMenyaPchela@users.noreply.github.com>
2022-06-15 15:51:04 +0500

    Update lab31.c

--- lab31.c
+++ lab31.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 #include <math.h>
-
 int main()
 {
-  // Шаг изменения аргумента
   double h;
-  printf("Введите шаг изменения аргумента -> ");
+  printf("Введите шаг изменения аргумента: ");
   scanf("%lf", &h);
-
-  // Вывод начала таблицы
   printf("%-20sf(x)\n", "x");
-  printf("--------------------\n");
-
-  // Расчет количества шагов
+  printf("-----------------------------\n");
   int count_h = (int)(2 / h);
   double y;
-
   int i = 0;
-  do
+  while (i <= count_h)
   {
     double x = i * h;
     if (x <= 1)
-      y = sqrt(x + 1) - sqrt(x) - (1\2);
+      y = cos(x)*exp(-pow((i * h), 2));
     else
-      y = -pow(exp, ((i * h) - (1 \ (i * h)));
-
+      y = log(x + 1) - sqrt(4 - pow(x, 2));
     printf("%-20lf% lf\n", x, y);
     i++;
   }
-  while (i <= count_h);
-
   return 0;
 }
